da9a79eea88fc89a8074c45191b29c01b864506f
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-08 20:54:00 -0400

    Started user entry system with hard code in ruby

--- city.rb
+++ city.rb
@@ -1,2 +1,26 @@
 #This will host the class that makes the object (City) you will work in
 
+class PresentDay
+	def initialize(city)
+		@city = city
+	end
+	
+	def user_input
+		puts "Enter your full name:"
+		@full_name = gets.to_s
+		puts "Enter your age: "
+		@age = gets.to_i
+	end
+
+	def display
+		puts "Name: #{@full_name}"
+		puts "Age: #{@age}"
+		puts "Currently living in #{@city}"
+	end
+end
+
+
+user1 = PresentDay.new("Toronto")
+user1.user_input
+user1.display
+
